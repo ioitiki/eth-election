90d4acb7c13d56bbcd48385f198a9c65ffafa923
Andrew Malkin <andymalkin@hotmail.com>
2018-03-21 19:19:30 -0700

    clean up code and add rinkeby contract address info

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,50 +1,50 @@
 pragma solidity ^0.4.2;
 
 contract Election {
-
+    // Model a Candidate
     struct Candidate {
         uint id;
         string name;
         uint voteCount;
     }
 
-    // Function for accessing each candidate
-    mapping(uint => Candidate) public candidates;
-
-    // Function to lookup who has voted
+    // Store accounts that have voted
     mapping(address => bool) public voters;
-
-    // Needed to keep track of the size of candidates
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
     uint public candidatesCount;
 
+    // voted event
     event votedEvent (
         uint indexed _candidateId
     );
 
-    function Election() public {
+    function Election () public {
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");
     }
 
-    function addCandidate(string _name) private {
-        candidatesCount++;
+    function addCandidate (string _name) private {
+        candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
-    function vote(uint _candidateId) public {
-        // Requre that they haven't voted before
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
         require(!voters[msg.sender]);
 
-        // Require that they are voting for a valid candidate
+        // require a valid candidate
         require(_candidateId > 0 && _candidateId <= candidatesCount);
 
-        // Record that voter has voted
+        // record that voter has voted
         voters[msg.sender] = true;
 
-        // Upadate our candidates vote count
+        // update candidate vote Count
         candidates[_candidateId].voteCount ++;
 
-        // Trigger votedEvent
+        // trigger voted event
         votedEvent(_candidateId);
     }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -2,21 +2,21 @@ App = {
   web3Provider: null,
   contracts: {},
   account: '0x0',
+  hasVoted: false,
 
-  // Initialize App using Web3
   init: function() {
     return App.initWeb3();
   },
 
-  // Connects client side application to the Ethereum blockchain
   initWeb3: function() {
-    if (typeof web3 !== 'undifined') {
-      // If MetaMask has provided a web3 instance
+    // TODO: refactor conditional
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
       App.web3Provider = web3.currentProvider;
       web3 = new Web3(web3.currentProvider);
     } else {
-      // Specify default incase web3 does not provide one
-      App.web3Provider = Web3.providers.HttpProvider('http://localhost:7574');
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
       web3 = new Web3(App.web3Provider);
     }
     return App.initContract();
@@ -26,7 +26,7 @@ App = {
     $.getJSON("Election.json", function(election) {
       // Instantiate a new truffle contract from the artifact
       App.contracts.Election = TruffleContract(election);
-      // Connect provider to interact with the contract
+      // Connect provider to interact with contract
       App.contracts.Election.setProvider(App.web3Provider);
 
       App.listenForEvents();
@@ -35,14 +35,18 @@ App = {
     });
   },
 
+  // Listen for events emitted from the contract
   listenForEvents: function() {
     App.contracts.Election.deployed().then(function(instance) {
+      // Restart Chrome if you are unable to receive this event
+      // This is a known issue with Metamask
+      // https://github.com/MetaMask/metamask-extension/issues/2393
       instance.votedEvent({}, {
-        // subscribe to events on the whole blockchain 0-latest
         fromBlock: 0,
         toBlock: 'latest'
       }).watch(function(error, event) {
         console.log("event triggered", event)
+        // Reload when a new vote is recorded
         App.render();
       });
     });
@@ -64,7 +68,7 @@ App = {
       }
     });
 
-    //Load contract data
+    // Load contract data
     App.contracts.Election.deployed().then(function(instance) {
       electionInstance = instance;
       return electionInstance.candidatesCount();
@@ -75,24 +79,24 @@ App = {
       var candidatesSelect = $('#candidatesSelect');
       candidatesSelect.empty();
 
-      for(var i = 1; i <= candidatesCount; i++) {
+      for (var i = 1; i <= candidatesCount; i++) {
         electionInstance.candidates(i).then(function(candidate) {
           var id = candidate[0];
           var name = candidate[1];
           var voteCount = candidate[2];
 
           // Render candidate Result
-          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>";
+          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
           candidatesResults.append(candidateTemplate);
 
           // Render candidate ballot option
-          var candidateOption = "<option value='" + id + "'>" + name + "</ option>";
+          var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
           candidatesSelect.append(candidateOption);
         });
       }
       return electionInstance.voters(App.account);
     }).then(function(hasVoted) {
-      // do not allow user to vote
+      // Do not allow a user to vote
       if(hasVoted) {
         $('form').hide();
       }
@@ -108,9 +112,9 @@ App = {
     App.contracts.Election.deployed().then(function(instance) {
       return instance.vote(candidateId, { from: App.account });
     }).then(function(result) {
-      // wait for votes to Upadate
-      $('#content').hide();
-      $('#loader').show();
+      // Wait for votes to update
+      $("#content").hide();
+      $("#loader").show();
     }).catch(function(err) {
       console.error(err);
     });
